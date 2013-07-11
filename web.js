81533975029086710afaba8d3c1effa258b68671
AlbertoPardo <flormarper82.gmail.com>
2013-07-11 10:32:10 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,9 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fs = require('fs');    
+  var buf = new Buffer (256);    
+  response.send(buf.toString('utf8',fs.readFileSync('index.html')));
 });
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
